c9bdd80b00ce8eb8ab7578cd48292a158529b4ba
kirankunigiri <kirankunigiri@gmail.com>
2016-12-17 20:44:29 -0800

    Small demo added

--- Family/Family/ViewController.swift
+++ Family/Family/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Outlets
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    // Properties
+    let family = Family(serviceType: "family-demo")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        textLabel.text = ""
+        textField.delegate = self
+        textField.returnKeyType = .done
+        
+        family.delegate = self
     }
 
 
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.textLabel.text = textField.text
+        if (textField.text != nil && textField.text!.characters.count > 0) {
+            family.sendData(object: textField.text!)
+        }
+        return true
+    }
+}
+
+extension ViewController: FamilyDelegate {
+    
+    func receivedData(data: Data) {
+        OperationQueue.main.addOperation {
+            let string = data.convert() as? String
+            self.textLabel.text = string
+        }
+    }
+    
+}
